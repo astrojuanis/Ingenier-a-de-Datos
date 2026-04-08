d743c63d29770e630150e9aa0389b6a545a8e815
astrojuanis <juanita.castrillon@urosario.edu.co>
2026-04-07 21:24:26 -0500

    RetoConsultas

---
+++ Corte 2/MySQL/tareaconsultas2.sql
@@ -0,0 +1,103 @@
+#Reto 1 
+##
+##  Crea las siguientes tablas empleados (id, nombre, deptoId, salario) producto (idproducto, nombreproducto, precioproducto, categoriaproducto)
+##departamento (idDepartamento, nombreDepartamento) ##
+CREATE DATABASE IF NOT EXISTS tienda2;
+USE tienda2;
+
+SET SQL_SAFE_UPDATES = 0;
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS empleados;
+DROP TABLE IF EXISTS productos;
+DROP TABLE IF EXISTS departamento;
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE departamento (
+  idDepartamento 	INT primary key auto_increment,
+  nombreDepartamento 	varchar(120) not null
+);
+
+	CREATE TABLE empleados (
+  idEmpleados		Int       PRIMARY KEY AUTO_INCREMENT,
+  nombreEmpleado	varchar (100) not null,
+  deptoIdFK 		INT 		null,
+  salarioEmpleado	decimal(10,2),
+  FOREIGN KEY (deptoIdFK) REFERENCES departamento (idDepartamento)
+);
+
+CREATE TABLE productos (
+  idProducto        INT          PRIMARY KEY AUTO_INCREMENT,
+  nombre_producto   VARCHAR(120) NOT NULL,
+  precioProducto    DECIMAL(10,2),
+  categoriaProducto VARCHAR(60)
+);
+
+
+
+##Reto 2 
+#Registrar 5 datos en empleados, 3 departamentos y 5 productos.
+
+DESCRIBE departamento;
+INSERT INTO departamento(idDepartamento, nombreDepartamento)
+VALUES 
+  (1220,'Administrativo'),
+  (3330,  'Ventas'),
+  (10, 'Gerencia');
+  Select * from departamento;
+
+DESCRIBE empleados;
+INSERT INTO empleados(nombreEmpleado,deptoIdFK, salarioEmpleado)
+VALUES ('Ana Garcia', 1220, 2300000),
+		('Julian	Medina', 3330, 3200000),
+         ('Ana Gara', 10, 250000),
+		('Carolina Jimenez', 1220,4000000),
+		('Alejandro Palacios', 3330,5000000);
+SELECT * FROM empleados;
+
+DESCRIBE productos;
+INSERT INTO productos (nombre_producto, precioProducto, categoriaProducto)
+VALUES 
+  ('Laptop Pro',   1200000,  'Electrónica'),
+  ('Mouse USB',      50000,  'Accesorios'),
+  ('Monitor 32"',  500000,   'Electrónica'),
+  ('Iphone 17Pro', 3800000,	'Electrónica'),
+  ('Teclados',     100000,  'Accesorios');
+  
+SELECT * FROM productos;
+
+  
+##Subconsulta
+###----where---
+select nombreEmpleado, salarioEmpleado
+from empleados
+where salarioEmpleado>
+	(select AVG(salarioEmpleado)
+	from empleados);
+###----where+in----
+select nombreEmpleado, salarioEmpleado
+from empleados
+where deptoIdFK in
+	(select idDepartamento
+	from departamento
+    where nombreDepartamento in ('Gerencia','Ventas'));
+    
+###----tabla derivada----
+select deptoIdFK, prom_salario
+from 
+  (select deptoIdFK, AVG(salarioEmpleado) as prom_salario
+  from empleados
+  group by deptoIdFK) as promedios
+where prom_salario > 450000;
+
+## Reto 
+select nombreEmpleado, salarioEmpleado as salario,
+(select avg(salarioEmpleado) from empleados) as promedioGeneral,
+salarioEmpleado - (select avg(salarioEmpleado) from empleados) AS Desviacion from empleados;
+
+##Tarea 
+
+select categoriaProducto, max(precioProducto) from productos
+where precioProducto > 
+	(select avg(precioProducto) from productos)
+    group by categoriaProducto
+    order by max(precioProducto) desc; 
